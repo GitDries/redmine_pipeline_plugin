bac7fa406bb3c9fe561e1c3b07091c5b0cdc6ddc
dries <yeah_driesje@yahoo.co.uk>
2013-12-15 01:28:29 +0100

    Fixes #5

--- app/controllers/pipeline_controller.rb
+++ app/controllers/pipeline_controller.rb
@@ -5,7 +5,7 @@ class PipelineController < ApplicationController
 
   before_filter :require_admin
   menu_item :pipeline
-  
+
   def report
     @users = User.active
     @statuses = IssueStatus.where(:is_closed => false)
@@ -21,21 +21,23 @@ class PipelineController < ApplicationController
 
     if params[:pipeline]
       @users_selected = params[:pipeline][:users]
+      @user_groups = Group.joins(:users).where('users_users.id IN (:u_ids)', :u_ids => @users).collect { |x| x.id }
       @statuses_selected = params[:pipeline][:statuses]
       @trackers_selected = params[:pipeline][:trackers]
       @projects_selected = params[:pipeline][:projects]
       @group = params[:pipeline][:group].to_sym if @grouping_options.include?(params[:pipeline][:group].to_sym)
     end
 
-	non_assigned_issues = @users.size==@users_selected.size ? " assigned_to_id IS NULL " : " 0=1 "
+	  non_assigned_issues = @users.size==@users_selected.size ? " assigned_to_id IS NULL " : " 0=1 "
     @todos = Issue.where('project_id in (:p_ids) ' +
                              'AND tracker_id IN (:t_ids) ' +
                              'AND status_id IN (:s_ids) ' +
-                             'AND ( assigned_to_id IN (:u_ids) OR '+non_assigned_issues+' )',
+                             'AND ( assigned_to_id IN (:u_ids) OR assigned_to_id IN (:ug_ids) OR '+non_assigned_issues+' )',
 	              :p_ids => @projects_selected,
 	              :t_ids => @trackers_selected,
-                  :s_ids => @statuses_selected,
-	              :u_ids => @users_selected
+                :s_ids => @statuses_selected,
+	              :u_ids => @users_selected,
+                :ug_ids => @user_groups
 			).includes(:tracker, :assigned_to, :project, :time_entries
 			).order('project_id ASC, status_id DESC, tracker_id ASC')
 
